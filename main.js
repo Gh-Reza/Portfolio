3c7bd518fcd02e42fb34addbe8397a385602ec89
Reza Merzaie <ghrezam97@gmail.com>
2023-05-16 03:22:12 +0430

    Added mobile menu

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 const menuButton = document.querySelector(".menu-icon");
-const menuLinks = document.querySelector(".menu");
+const menu = document.querySelector(".menu");
+const close = document.createElement('li');
+close.className = "close";
+const closeIcon = document.createElement('img');
+closeIcon.className = "close-icon";
+closeIcon.src = "./img/close.svg"
+close.append(closeIcon);
+const menuItem = document.querySelectorAll('.menu-item');
 
 menuButton.addEventListener("click", () => {
-  menuLinks.classList.toggle("menu-mobile");
+  menu.classList.add("menu-mobile");
+  menu.append(close);
 });
+
+close.addEventListener("click", () => {
+  menu.classList.remove("menu-mobile");
+});
+
+menuItem.forEach(element => { 
+  element.addEventListener("click", () => {
+    menu.classList.remove('menu-mobile');
+  });
+});
+
+
