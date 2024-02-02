c17c45d3db290798841d094cbfffa98bb27d6cd7
reza <ghrezam97@gmail.com>
2024-02-02 20:41:36 +0430

    Add projects

--- js/data.js
+++ js/data.js
@@ -1,112 +1,87 @@
 const cardInfo = [
   {
     id: '0',
-    imgPath: 'img/multi.png',
-    cardMobileTitle: 'Multi-Post Stories',
-    cardDeskTitle: 'Multi-Post Stories',
-    mobileDescription: `A daily selection of privately personalized reads; no accounts or
-    sign-ups required. has been the industry's standard dummy text
-    ever since the 1500s, when an unknown printer took a standard
-    dummy text.`,
-    deskDescription: `A daily selection of privately personalized reads; no accounts or
-    sign-ups required. has been the industry's standard dummy text
-    ever since the 1500s, when an unknown printer took a standard
-    dummy text.`,
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.<br><br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-    tech: '<li><a href="#">HTML</a></li><li><a href="#">CSS</a></li><li><a href="#">Bootstrap</a></li><li><a href="#">Ruby</a></li>',
-    liveLink: 'https://gh-reza.github.io/Portfolio-mobile-version/',
-    sourceLink: 'https://github.com/Gh-Reza/Portfolio-mobile-version',
+    imgPath: 'img/WeatherApp.png',
+    cardMobileTitle: 'WeatherApp',
+    cardDeskTitle: 'WeatherApp',
+    mobileDescription: `A weather application that provides information about the weather condition of any city in the world. Built with React and Redux.`,
+    deskDescription: `A weather application that provides information about the weather condition of any city in the world. Built with React and Redux`,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    longDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.<br><br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    tech: '<li><a href="#">React</a></li><li><a href="#">Redux</a></li><li><a href="#">CSS</a></li>',
+    liveLink: 'https://github.com/Gh-Reza/weather-app',
+    sourceLink: 'https://github.com/Gh-Reza/weather-app',
   },
   {
     id: '1',
-    imgPath: 'img/card4.svg',
-    cardMobileTitle: 'Profesional Art Printing Data',
-    mobileDescription: `A daily selection of privately personalized reads; no accounts
-    or sign-ups required. Has been the industry's standard.`,
-    cardDeskTitle: 'Data Dashboard Healthcare',
-    deskDescription: `A daily selection of privately personalized reads; no accounts
-    or sign-ups required. Has been the industry's standard.`,
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.<br><br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-    tech: '<li><a href="#">HTML</a></li><li><a href="#">CSS</a></li><li><a href="#">Bootstrap</a></li>',
-    liveLink: 'https://gh-reza.github.io/Portfolio-mobile-version/',
-    sourceLink: 'https://github.com/Gh-Reza/Portfolio-mobile-version',
+    imgPath: 'img/SpaceTraveler.png',
+    cardMobileTitle: "Space Traveler's Hub",
+    mobileDescription: `This exciting project aims to simulate SpaceX missions and allow users to reserve rockets for their personalized space adventures. Built with React/Redux and CSS.`,
+    cardDeskTitle: "Space Traveler's Hub",
+    deskDescription: `This exciting project aims to simulate SpaceX missions and allow users to reserve rockets for their personalized space adventures. Built with React/Redux and CSS.`,
+    description: `This exciting project aims to simulate SpaceX missions and allow users to reserve rockets for their personalized space adventures. Built with React/Redux and CSS.`,
+    longDescription: `This exciting project aims to simulate SpaceX missions and allow users to reserve rockets for their personalized space adventures. Built with React/Redux and CSS.`,
+    tech: '<li><a href="#">React</a></li><li><a href="#">Redux</a></li><li><a href="#">CSS</a></li>',
+    liveLink: 'https://space-travelers-hub-fl03.onrender.com/',
+    sourceLink: 'https://github.com/Gh-Reza/space-traveler-s-hub',
   },
   {
     id: '2',
-    imgPath: 'img/card2.svg',
-    cardMobileTitle: 'Profesional Art Printing Data',
-    mobileDescription: `A daily selection of privately personalized reads; no accounts
-    or sign-ups required. Has been the industry's standard.`,
-    cardDeskTitle: 'Data Dashboard Healthcare',
-    deskDescription: `A daily selection of privately personalized reads; no accounts
-    or sign-ups required. Has been the industry's standard.`,
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.<br><br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-    tech: '<li><a href="#">HTML</a></li><li><a href="#">CSS</a></li><li><a href="#">Bootstrap</a></li>',
-    liveLink: 'https://gh-reza.github.io/Portfolio-mobile-version/',
-    sourceLink: 'https://github.com/Gh-Reza/Portfolio-mobile-version',
+    imgPath: 'img/BookStore.png',
+    cardMobileTitle: 'Bookstore',
+    mobileDescription: `Welcome to Bookstore, your ultimate web app for seamlessly organizing and tracking your book collection! With Bookstore, you can effortlessly store and manage information about your beloved books, including title, author, category, and even your reading progress.`,
+    cardDeskTitle: 'Bookstore',
+    deskDescription: `Welcome to Bookstore, your ultimate web app for seamlessly organizing and tracking your book collection! With Bookstore, you can effortlessly store and manage information about your beloved books, including title, author, category, and even your reading progress.`,
+    description: `Welcome to Bookstore, your ultimate web app for seamlessly organizing and tracking your book collection! With Bookstore, you can effortlessly store and manage information about your beloved books, including title, author, category, and even your reading progress.`,
+    longDescription: `Welcome to Bookstore, your ultimate web app for seamlessly organizing and tracking your book collection! With Bookstore, you can effortlessly store and manage information about your beloved books, including title, author, category, and even your reading progress.`,
+    tech: '<li><a href="#">React</a></li><li><a href="#">Redux</a></li><li><a href="#">CSS</a></li>',
+    liveLink: 'https://book-store-af6j.onrender.com/',
+    sourceLink: 'https://github.com/Gh-Reza/bookstore/',
   },
   {
     id: '3',
-    imgPath: 'img/card3.svg',
-    cardMobileTitle: 'Profesional Art Printing Data',
-    mobileDescription: `A daily selection of privately personalized reads; no accounts
-    or sign-ups required. Has been the industry's standard.`,
-    cardDeskTitle: 'Website Portfolio',
-    deskDescription: `A daily selection of privately personalized reads; no accounts
-    or sign-ups required. Has been the industry's standard.`,
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.<br><br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-    tech: '<li><a href="#">HTML</a></li><li><a href="#">CSS</a></li><li><a href="#">Bootstrap</a></li>',
-    liveLink: 'https://gh-reza.github.io/Portfolio-mobile-version/',
-    sourceLink: 'https://github.com/Gh-Reza/Portfolio-mobile-version',
+    imgPath: 'img/TvMaz.png',
+    cardMobileTitle: 'TV Shows',
+    mobileDescription: `About
+    This project aims to provide information about different TV shows, such as: Summary, genre, and ImDB ratings. Built with HTML, CSS, and JavaScript.`,
+    cardDeskTitle: 'TV Shows',
+    deskDescription: `
+    This project aims to provide information about different TV shows, such as: Summary, genre, and ImDB ratings. Built with HTML, CSS, and JavaScript.`,
+    description: `This project aims to provide information about different TV shows, such as: Summary, genre, and ImDB ratings. Built with HTML, CSS, and JavaScript.`,
+    longDescription: `This project aims to provide information about different TV shows, such as: Summary, genre, and ImDB ratings. Built with HTML, CSS, and JavaScript.`,
+    tech: '<li><a href="#">HTML</a></li><li><a href="#">CSS</a></li><li><a href="#">JavaScript</a></li>',
+    liveLink: 'https://demesameneshoa.github.io/JavaScript-Capstone/',
+    sourceLink: 'https://github.com/Gh-Reza/JavaScript-Capstone',
   },
   {
     id: '4',
-    imgPath: 'img/card4.svg',
-    cardMobileTitle: 'Profesional Art Printing Data',
-    mobileDescription: `A daily selection of privately personalized reads; no accounts
-    or sign-ups required. Has been the industry's standard.`,
-    cardDeskTitle: 'Data Dashboard Healthcare',
-    deskDescription: `A daily selection of privately personalized reads; no accounts
-    or sign-ups required. Has been the industry's standard.`,
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.<br><br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-    tech: '<li><a href="#">HTML</a></li><li><a href="#">CSS</a></li><li><a href="#">Bootstrap</a></li>',
-    liveLink: 'https://gh-reza.github.io/Portfolio-mobile-version/',
-    sourceLink: 'https://github.com/Gh-Reza/Portfolio-mobile-version',
+    imgPath: 'img/CTS.png',
+    cardMobileTitle: 'CTS 2023',
+    mobileDescription: `This is my first capstone project at Microverse. This website is about to provide people with information about a climate technology show. Built with HTML, CSS, and JavaScript`,
+    cardDeskTitle: 'CTS 2023',
+    deskDescription: `This is my first capstone project at Microverse. This website is about to provide people with information about a climate technology show. Built with HTML, CSS, and JavaScript`,
+    description: `This is my first capstone project at Microverse. This website is about to provide people with information about a climate technology show. Built with HTML, CSS, and JavaScript`,
+    longDescription: `This is my first capstone project at Microverse. This website is about to provide people with information about a climate technology show. Built with HTML, CSS, and JavaScript`,
+    tech: '<li><a href="#">HTML</a></li><li><a href="#">CSS</a></li><li><a href="#">JavaScript</a></li>',
+    liveLink: 'https://gh-reza.github.io/CTS2023/',
+    sourceLink: 'https://github.com/Gh-Reza/CTS2023',
   },
   {
     id: '5',
-    imgPath: 'img/card5.svg',
-    cardMobileTitle: 'Profesional Art Printing Data',
-    mobileDescription: `A daily selection of privately personalized reads; no accounts
-    or sign-ups required. Has been the industry's standard.`,
-    cardDeskTitle: 'Data Dashboard Healthcare',
-    deskDescription: `A daily selection of privately personalized reads; no accounts
-    or sign-ups required. Has been the industry's standard.`,
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.<br><br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-    tech: '<li><a href="#">HTML</a></li><li><a href="#">CSS</a></li><li><a href="#">Bootstrap</a></li>',
-    liveLink: 'https://gh-reza.github.io/Portfolio-mobile-version/',
-    sourceLink: 'https://github.com/Gh-Reza/Portfolio-mobile-version',
-  },
-  {
-    id: '6',
-    imgPath: 'img/card6.svg',
-    cardMobileTitle: 'Profesional Art Printing Data',
-    mobileDescription: `A daily selection of privately personalized reads; no accounts
-    or sign-ups required. Has been the industry's standard.`,
-    cardDeskTitle: 'Website Portfolio',
-    deskDescription: `A daily selection of privately personalized reads; no accounts
-    or sign-ups required. Has been the industry's standard.`,
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.<br><br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-    tech: '<li><a href="#">HTML</a></li><li><a href="#">CSS</a></li><li><a href="#">Bootstrap</a></li>',
-    liveLink: 'https://gh-reza.github.io/Portfolio-mobile-version/',
-    sourceLink: 'https://github.com/Gh-Reza/Portfolio-mobile-version',
+    imgPath: 'img/MyBudget.png',
+    cardMobileTitle: 'My Budget App',
+    mobileDescription: `A streamlined budgeting app designed for effortless expense tracking. Categorize spending by brands, gaining valuable insights into your financial habits. Take control of your finances with simplicity and ease.`,
+    cardDeskTitle: 'My Budget App',
+    deskDescription: `A streamlined budgeting app designed for effortless expense tracking. Categorize spending by brands, gaining valuable insights into your financial habits. Take control of your finances with simplicity and ease.`,
+    description:
+      'A streamlined budgeting app designed for effortless expense tracking. Categorize spending by brands, gaining valuable insights into your financial habits. Take control of your finances with simplicity and ease.',
+    longDescription:
+      'A streamlined budgeting app designed for effortless expense tracking. Categorize spending by brands, gaining valuable insights into your financial habits. Take control of your finances with simplicity and ease.',
+    tech: '<li><a href="#">Ruby on Rails</a></li>',
+    liveLink: 'https://mybudget-c56c.onrender.com/',
+    sourceLink: 'https://github.com/Gh-Reza/Budget-app/',
   },
 ];
 
